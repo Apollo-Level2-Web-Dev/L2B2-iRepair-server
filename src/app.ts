4410dab4219a57bb982237b14bfe661ccc33c6d0
mir-hussain <mir.hm110@gmail.com>
2024-01-26 00:42:33 +0600

    init

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app: Application = express();
 //parsers
 app.use(express.json());
 
-app.use(cors({ origin: ['http://localhost:3000'] }));
+app.use(cors({ origin: '*' }));
 
 // application routes
 app.use('/api/v1', router);
